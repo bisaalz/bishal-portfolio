207bb57f22735de81925f8d8a9589efab53f97b2
bishalghimire007 <bishal@generaltechnology.com.np>
2023-07-11 17:58:15 +0545

    scroll design and hero section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Image from 'next/image'
-import herobg from  '@/../public/herobg.jpg'
+import React from "react";
+import Image from "next/image";
+import herobg from "@/../public/herobg.jpg";
 
 const Navbar = () => {
   return (
-    <div className='heroBackground  bg-gradient-to-r from-slate-200 to-orange-50'>
-
+    <div className="heroBackground bg-gradient-to-r from-slate-200 to-orange-50">
+      <div className="container">Navbar</div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
