2740ea5e5a3c478ffa86c50d237b7a136305038b
bishalghimire007 <bishal@generaltechnology.com.np>
2023-07-12 16:08:25 +0545

    some changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,20 +8,20 @@ const Navbar = () => {
         <div className="heroBackground bg-gradient-to-r from-[#0B1062] to-[#710834] bg-no-repear bg-center bg-cover">
           <div className="bg-[#F0F6FD] z-40 2xl:h-[68px] h-[68px] boxShadow sticky top-0 text-white">
             <div className="container">
-              <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+              <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a
                   href="https://bishalghimire007.netlify.app/"
-                  class="flex items-center"
+                  className="flex items-center"
                 >
-                  <span class="font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-[#ffffff] to-[#ffffff]">
+                  <span className="font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-[#ffffff] to-[#ffffff]">
                     Bis<span className="text-[#DC2626]">hal</span>
                   </span>
                 </a>
-                <div class="flex md:order-2">
+                <div className="flex md:order-2">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     style={{ transform: "none", transformOrigin: "12px 12px" }}
-                    class="h-6 w-6 mt-2 md:mt-0"
+                    className="h-6 w-6 mt-2 md:mt-0"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -37,13 +37,13 @@ const Navbar = () => {
                   <button
                     data-collapse-toggle="navbar-sticky"
                     type="button"
-                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                     aria-controls="navbar-sticky"
                     aria-expanded="false"
                   >
-                    <span class="sr-only">Open main menu</span>
+                    <span className="sr-only">Open main menu</span>
                     <svg
-                      class="w-5 h-5"
+                      className="w-5 h-5"
                       aria-hidden="true"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
@@ -60,14 +60,14 @@ const Navbar = () => {
                   </button>
                 </div>
                 <div
-                  class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+                  className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
                   id="navbar-sticky"
                 >
-                  <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:border-gray-700">
+                  <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:border-gray-700">
                     <li>
                       <a
                         href="#"
-                        class="block py-2 pl-3 pr-4 text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#F55F29] md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                        className="block py-2 pl-3 pr-4 text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#F55F29] md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                       >
                         HOME
                       </a>
@@ -75,7 +75,7 @@ const Navbar = () => {
                     <li>
                       <a
                         href="#"
-                        class="block py-2 pl-3 pr-4 text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#F55F29] md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                        className="block py-2 pl-3 pr-4 text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#F55F29] md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                       >
                         ABOUT
                       </a>
@@ -83,7 +83,7 @@ const Navbar = () => {
                     <li>
                       <a
                         href="#"
-                        class="block py-2 pl-3 pr-4 text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#F55F29] md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                        className="block py-2 pl-3 pr-4 text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#F55F29] md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                       >
                         MY PROJECT
                       </a>
@@ -91,7 +91,7 @@ const Navbar = () => {
                     <li>
                       <a
                         href="#"
-                        class="block py-2 pl-3 pr-4 text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#F55F29] md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                        className="block py-2 pl-3 pr-4 text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#F55F29] md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                       >
                         CONTACT
                       </a>
@@ -104,7 +104,7 @@ const Navbar = () => {
         </div>
         <div className="absolute top-[40%] left-[12%] container">
           <div className="grid grid-cols-1 md:grid-cols-2">
-            <div className="space-y-2">
+            <div className="">
               <h6 className="text-[#ffffff] font-medium">
                 HELLO THERE, WELCOME TO MY SITE
               </h6>
@@ -112,7 +112,7 @@ const Navbar = () => {
                 I'am Bishal Ghimire
               </span>
               <h2
-                class="font-medium text-4xl md:text-5xl xl:text-7xl !leading-[114%] text-[#ffffff] "
+                className="font-medium text-4xl md:text-5xl xl:text-7xl !leading-[114%] text-[#ffffff] "
                 style={{ opacity: "1", transform: "none" }}
               >
                 <span className="text-[#DC2626]">A Frontend</span> &amp; UI/UX
