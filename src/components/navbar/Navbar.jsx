67e1cce13fc56477730d52a4cb801a18f7290fd2
bishalghimire007 <bishal@generaltechnology.com.np>
2023-07-12 11:17:44 +0545

    some mionor changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,9 +4,11 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <div className="heroBackground bg-gradient-to-r from-red-500 to-orange-500 bg-no-repear bg-center bg-cover">
-      <div className="bg-[#F0F6FD] z-40 2xl:h-[64px] h-[64px] bg-opacity-[.40] backdrop-blur-lg boxShadow sticky top-0">
+      <div className="bg-[#F0F6FD] z-40 2xl:h-[68px] h-[68px] bg-opacity-[.50] backdrop-blur-lg boxShadow sticky top-0">
         <div className="container">
-          test
+          <div className="Logo">1</div>
+          <div className="Menu">2</div>
+          <div className="Toggle">3</div>
         </div>
       </div>
     </div>
